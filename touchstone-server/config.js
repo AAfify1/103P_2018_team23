25084ec098d90614e27d6e05b3ee99685d81bb4c
Sam <gaoshan_274@hotmail.com>
2018-04-27 19:32:26 +0100

    updated for azure db

--- touchstone-server/config.js
+++ touchstone-server/config.js
@@ -1,15 +1,19 @@
 //UPDATE THESE VALUES WHEN DEPLOYING
+var fs = require("fs");
 
 var db_config = {
-	host: "localhost",
-	user: "sam",
+	host: "touchstonedb.mysql.database.azure.com",
+	user: "sam@touchstonedb",
 	password: "COMP103Ppassword",
-	database: "touchstone"
+	database: "touchstone",
+	ssl: {
+		ca: fs.readFileSync(__dirname + "/BaltimoreCyberTrustRoot.crt.pem")
+	}
 };
 
 var azure_config = {
 	accountName: "touchstonedata",
-	accountKey: "access-key"
+	accountKey: "Izp0WBwA2CpnRpFw6WhVONSHDXPrxFg6gKLC+aXtVWMyfwzfmGWmh+pQQ2J9b+eWz0TJK32TpSQY0IhoA63GGQ=="
 };
 
 module.exports = {
