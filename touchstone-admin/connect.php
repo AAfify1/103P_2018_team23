642feff7b49e5fb4dc51cd31e0fe9b5c6040f4af
Sam <gaoshan_274@hotmail.com>
2018-04-27 20:34:23 +0100

    updated admin interface to use azure sql

--- touchstone-admin/connect.php
+++ touchstone-admin/connect.php
@@ -1,7 +1,9 @@
 <?php
-	$MYSQL_HOST = "localhost";
-	$MYSQL_USER = "sam"; //update this!
-	$MYSQL_PASSWORD = "COMP103Ppassword"; //update this!
-	$MYSQL_DB = "touchstone";
-    $sql = mysqli_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DB);
+    $MYSQL_HOST = "touchstonedb.mysql.database.azure.com";
+    $MYSQL_USER = "sam@touchstonedb"; //update this!
+    $MYSQL_PASSWORD = "COMP103Ppassword"; //update this!
+    $MYSQL_DB = "touchstone";
+    $sql = mysqli_init();
+    $sql -> ssl_set(NULL, NULL, "./BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+    $sql->real_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DB);
 ?>
